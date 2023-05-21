1dfd3058dc280929afd1a1525ad289b8a67acd10
felix corke <felix@blackspike.com>
2023-05-21 20:31:01 +0100

    removed about modal and reinstated router/page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { dev } from '$app/environment'
+  import '../assets/css/main.scss'
+</script>
+
+<svelte:head>
+  {#if !dev}
+    <script defer data-domain="ogvideo.app" src="/js/script.js"></script>
+  {/if}
+</svelte:head>
+
+<slot />
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,114 @@
+<script>
+  import Footer from '../../components/Footer.svelte'
+</script>
+
+<div class="container">
+  <main class="panel about vstack gap-7">
+    <section class="vstack gap-3">
+      <h2>WTF is OG Video</h2>
+      <p>
+        Enter your blog post url and it'll try and grab the meta title, description and share image,
+        and let you create and export an animated video for your socials!
+      </p>
+    </section>
+
+    <!-- footer -->
+    <div class="panel panel-sm">
+      <Footer>
+        <a class="btn credits" href="/demo.mp4">Example Video</a>
+        <a class="btn credits" href="https://github.com/blackspike/ogvideo">Source on GitHub</a>
+        <a class="btn credits" href="https://www.blackspike.com">by blackspike.com</a>
+      </Footer>
+    </div>
+    <!-- Faq -->
+    <section class="vstack gap-3">
+      <h2>FAQ</h2>
+      <dl class="vstack gap-2">
+        <div class="panel vstack gap-2">
+          <dt>The url import didn't work</dt>
+          <dd>Yeah, it only works like 50% of the time. Hobby project!</dd>
+        </div>
+
+        <div class="panel vstack gap-2">
+          <dt>There's a weird bug</dt>
+          <dd>Try reloading the page!</dd>
+        </div>
+
+        <div class="panel vstack gap-2">
+          <dt>Can I add posts via query parameter</dt>
+          <dd>
+            <p>Why yes! Just add <code>?url=https://yoursite.com</code> to the url</p>
+
+            <p>
+              <a href="https://ogvideo.app/?url=https://www.bbc.co.uk/news/business-65644340"
+                >ogvideo.app/?url=https://bbc.co.uk</a
+              >
+            </p>
+            <p>
+              <a
+                href="https://ogvideo.app/?url=https://www.theonion.com/if-pigs-are-so-smart-why-won-t-they-debate-me-1850447712"
+                >ogvideo.app/?url=https://theonion.com</a
+              >
+            </p>
+          </dd>
+        </div>
+
+        <div class="panel vstack gap-2">
+          <dt>I'd prefer an <code>MP4</code> rather than a <code>WEBM</code></dt>
+          <dd>Me too buddy! Blame the suits. A workaround is to run this app in Safari.</dd>
+        </div>
+
+        <div class="panel vstack gap-2">
+          <dt>There are horizontal scrollbars on mobile, for shame.</dt>
+          <dd>I know ☹️ – not very responsive of us. Hobby project!</dd>
+        </div>
+
+        <div class="panel vstack gap-2">
+          <dt>I have a suggestion/question/abusive message</dt>
+          <dd>
+            Ping us on <a href="https://mastodon.cloud/@blackspike">mastodon</a> or at
+            <a href="http://www.blackspike.com">blackspike.com</a>
+          </dd>
+        </div>
+      </dl>
+    </section>
+
+    <footer class="vstack gap-3">
+      <p><a class="btn" href="/">← Cool</a></p>
+    </footer>
+  </main>
+</div>
+
+<style>
+  .container {
+    width: 100%;
+    display: grid;
+    place-content: center;
+  }
+  main {
+    inline-size: min(90vw, 45rem);
+    margin-inline: auto;
+    margin-block: var(--size-fluid-5);
+    padding: 2vw;
+    font-size: var(--font-size-1);
+    font-weight: var(--font-weight-4);
+    line-height: var(--font-lineheight-3);
+  }
+  dt {
+    font-size: var(--font-size-3);
+  }
+  p,
+  dd {
+    font-family: 'Inter';
+    color: var(--gray-3);
+  }
+  main .panel {
+    background-color: var(--gray-10);
+  }
+  a {
+    text-decoration: none;
+  }
+  .credits {
+    width: 100%;
+  }
+</style>
